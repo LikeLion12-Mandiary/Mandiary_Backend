1ef8b09b6f0da71d10716aaa65018718accc0ecc
주현지 <hyjoo0909@naver.com>
2024-07-25 15:46:08 +0900

    fix: model 연결

--- mandalarts/models.py
+++ mandalarts/models.py
@@ -2,17 +2,19 @@ from django.db import models
 
 from users.models import User
 
-class Mandalart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(default='만다라트')
-    created_at = models.DateField(auto_now_add=True)
-    is_achieved = models.BooleanField(default=False)
-
-class Goal(models.Model):
-    main_goal = models.CharField(null=True)
-    is_achieved = models.BooleanField(default=False)
-
 class SubGoal(models.Model):
     content = models.TextField(null=True)
     image= models.ImageField(upload_to='mandalart')
-    is_achieved = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
+
+class Goal(models.Model):
+    main_goal = models.CharField(null=True)
+    sub_goal = models.ManyToManyField(SubGoal)
+    completed = models.BooleanField(default=False)
+
+class Mandalart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(default='만다라트')
+    goal = models.ManyToManyField(Goal)
+    created_at = models.DateField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
