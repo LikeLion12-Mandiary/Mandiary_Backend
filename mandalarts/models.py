c0305cb2b2439493c4f0714a88b8b3069a2e0d98
주현지 <hyjoo0909@naver.com>
2024-07-27 00:48:49 +0900

    feat: 만다라트 모델 생성

--- mandalarts/models.py
+++ mandalarts/models.py
@@ -2,19 +2,36 @@ from django.db import models
 
 from users.models import User
 
-class SubGoal(models.Model):
-    content = models.TextField(null=True)
-    image= models.ImageField(upload_to='mandalart')
-    completed = models.BooleanField(default=False)
-
-class Goal(models.Model):
-    main_goal = models.CharField(null=True)
-    sub_goal = models.ManyToManyField(SubGoal)
-    completed = models.BooleanField(default=False)
-
 class Mandalart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(default='만다라트')
-    goal = models.ManyToManyField(Goal)
+    table_name = models.CharField(max_length=18, unique=True, default='')
+    title = models.CharField(null=True, max_length=18, default='')
     created_at = models.DateField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if not self.table_name:
+            self.table_name= self.generate_table_name()
+        super().save(*args, **kwargs)
+        self.create_goals()
+
+    def create_goals(self):
+        for i in range(8):
+            goal = Goal.objects.create(final_goal=self)
+            for i in range(8):
+                subgoal= SubGoal.objects.create(goal=goal)
+
+    def generate_table_name(self):
+        count = Mandalart.objects.filter(user=self.user).count()+1
+        return f"테이블명{count}"
+
+class Goal(models.Model):
+    final_goal = models.ForeignKey(Mandalart, on_delete= models.CASCADE, default='')
+    title = models.CharField(null=True, blank=True, max_length=18, default='')
+    completed = models.BooleanField(default=False)
+
+class SubGoal(models.Model):
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE, default='')
+    title = models.CharField(null=True, blank=True, max_length=18, default='')
+    image= models.ImageField(upload_to='mandalart/', null=True, blank=True)
     completed = models.BooleanField(default=False)
