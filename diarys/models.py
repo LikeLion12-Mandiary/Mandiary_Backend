d70063ec5508b9da553951bd919c3581e564b111
seo hyun seok <tjgustjr33@naver.com>
2024-07-30 22:46:07 +0900

    refactor : 사진 저장 파일 위치 수정
    user_None으로 표기 된점 발견하여 수정

--- diarys/models.py
+++ diarys/models.py
@@ -3,7 +3,7 @@ from users.models import User
 
 def diary_directory_path(instance, filename):
     # 파일을 'user_<id>/<filename>' 경로에 업로드합니다.
-    return f'user_{instance.id}/diary/{filename}'
+    return f'user_{instance.user.id}/diary/{filename}'
 
 class Diary(models.Model):
     content = models.TextField(max_length=1000, null=True, blank=True)
