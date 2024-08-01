55080d4e4da234aaaccfd351d99508556395e088
주현지 <hyjoo0909@naver.com>
2024-08-01 14:44:02 +0900

    chore: view impot 추가

--- mandalarts/views.py
+++ mandalarts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import generics
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
 from rest_framework import status
 
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from mandalarts.models import Badge, BadgeUnlock, Goal, Mandalart, Notification
 from mandalarts.serializers import *
