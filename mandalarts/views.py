5c23e4577c8dfada79279e394787674c4fd1345f
주현지 <hyjoo0909@naver.com>
2024-08-01 14:50:03 +0900

    fix: badge, noti view 수정

--- mandalarts/views.py
+++ mandalarts/views.py
@@ -143,42 +143,77 @@ def update_mandalart_status(mandalart):
         mandalart.completed = True
     mandalart.save()
 
-#관리자(뱃지 생성)
+#관리자(뱃지 생성) 
 "badge/"
 class BadgeCreateView(generics.CreateAPIView):
+    permission_classes=[IsOwnerOrReadOnly, IsAuthenticated]
     serializer_class=BadgeSerializer
     queryset=Badge.objects.all()
 
 #모든 뱃지
 "mybadge/"
 class BadgeView(generics.ListAPIView):
-    serializer_class=BadgeSerializer
-    queryset=Badge.objects.all()
-
+    permission_classes=[IsOwnerOrReadOnly, IsAuthenticated]
+    serializer_class=UserBadgeSerializer
+    def get_queryset(self):
+        return UserBadge.objects.filter(user=self.request.user)
+    
 #뱃지 잠금 해제
-"badgeUnlock/<int:badge_unlock_id>/<int:badge_id>/"
+"badgeUnlock/<int:badge_id>/"
 class BadgeUnlockView(generics.UpdateAPIView):
+    permission_classes=[IsAuthenticated]
     serializers_class = BadgeUnlockSerializer
-    queryset=BadgeUnlock.objects.all()
+
+    def get_queryset(self):
+        return BadgeUnlock.objects.filter(user=self.request.user, is_unlocked=False).order_by('id')
     
     def update(self, request, *args, **kwargs):
-        badge_unlock_id= kwargs.get('badge_unlock_id')
         badge_id= kwargs.get('badge_id')
         
-        #BadgeUnlock
         try:
-            badge_unlock= BadgeUnlock.objects.get(id=badge_unlock_id, is_unlocked=False) #나중에 user=request.user 추가
+            badge_unlock = self.get_queryset().first()
+            if not badge_unlock:
+                return Response({"message": "잠금 해제할 BadgeUnlock이 없습니다."}, status=status.HTTP_404_NOT_FOUND)
         except BadgeUnlock.DoesNotExist:
-            return Response({"message":"올바르지 않는 Badge Unlock id입니다."}, status=status.HTTP_400_BAD_REQUEST)
-        
+            return Response({"message": "BadgeUnlock을 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+
         # 잠금해제 할 Badge
         try:
-            badge = Badge.objects.get(id=badge_id) 
-            if badge.unlocked == True:
+            user_badge = UserBadge.objects.get(id=badge_id, user=self.request.user) 
+            if user_badge.unlocked == True:
                 return Response({"message":"잠금 해제가 이미 되어있는 뱃지입니다."}, status=status.HTTP_400_BAD_REQUEST)
         except Badge.DoesNotExist:
             return Response({"message":"존재하지 않는 뱃지입니다."}, status=status.HTTP_400_BAD_REQUEST)
         
+        # goal = Goal.objects.filter(user__goal=request.user, completed=True).exclude(
+        #     id__in=UserBadge.objects.filter(user=request.user, badge=user_badge.badge).values_list('goal_id', flat=True)
+        # ).first()
+
+        user=self.request.user
+
+        mandalarts = Mandalart.objects.filter(user=user)
+
+        goals = Goal.objects.filter(final_goal__in=mandalarts, completed=True)
+        print(goals)
+
+        # 잠금 해제된 목표를 제외
+        excluded_goal_ids = UserBadge.objects.filter(user=user, badge=user_badge.badge).values_list('goal_id', flat=True)
+        print(excluded_goal_ids)
+
+        excluded_goal_ids = list(excluded_goal_ids)  # queryset 빈 리스트 처리
+        filtered_goals = goals.exclude(id__in=excluded_goal_ids)
+        print("filtered_goals:",filtered_goals)
+        
+        # # 필터링된 Goal 중 제외된 목표를 제외합니다
+        # filtered_goals = goals.exclude(id__in=excluded_goal_ids)
+        # print(filtered_goals)
+
+        # 첫 번째 필터링된 Goal을 가져옵니다
+        goal = filtered_goals.first()
+
+        if not goal:
+            return Response({"message": "뱃지를 잠금 해제할 수 있는 완료된 목표가 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
         badge_unlock.is_unlocked = True
         badge_unlock.save()
 
@@ -186,73 +221,72 @@ class BadgeUnlockView(generics.UpdateAPIView):
         notification.unlockable_badge_count -= 1
         if notification.unlockable_badge_count == 0:
             notification.is_read = True
+        notification.message = f"목표를 달성하셨습니다 :) {notification.unlockable_badge_count}개의 뱃지를 잠금해제 해보세요!"
         notification.save()
 
-        badge.unlocked = True
-        badge.save()
+        user_badge.unlocked = True
+        user_badge.unlocked_at = timezone.now()
+        user_badge.goal = goal
+        user_badge.save()
 
         return Response({"message":"뱃지가 잠금해제 되었습니다."}, status=status.HTTP_200_OK)
 
-#잠금 해제한 뱃지
+#잠금 해제한 뱃지 
 "UnlockedBadge/"
 class UnlockedBadgeView(generics.ListAPIView):
-    serializer_class=BadgeSerializer
-    queryset=Badge.objects.filter(unlocked=True)
+    permission_classes=[IsAuthenticated]
+
+    serializer_class=UserBadgeSerializer
+    def get_queryset(self):
+        return UserBadge.objects.filter(user=self.request.user, unlocked=True)
 
 
-#나의 칭호
+#나의 칭호 
 "badgeTitle/"
 class BadgeTitleView(generics.ListAPIView):
-    serializer_class=BadgeTitleSerializer
-    queryset=Badge.objects.filter(unlocked=True)
+    permission_classes=[IsAuthenticated]
+
+    serializer_class=UserBadgeTitleSerializer
+    def get_queryset(self):
+        return UserBadge.objects.filter(user=self.request.user, unlocked=True)
 
 
 #랜덤칭호 표시
+"dailyBadge/"
+class DailyBadgeTitleView(generics.RetrieveAPIView):
+    permission_classes=[IsAuthenticated]
 
+    serializer_class=DailyBadgeSerializer
 
+    def get(self, request, *args, **kwargs):
+        user=request.user
+        print(user)
+        daily_badge= set_daily_badge_title(user)
+        serializer = self.get_serializer(daily_badge)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
+def set_daily_badge_title(user):
+    today = timezone.now().date()
+    daily_badge, created = DailyBadge.objects.get_or_create(user=user)
 
+    unlocked_badges = list(UserBadge.objects.filter(user=user, unlocked=True).values_list('badge', flat=True))
 
+    if created or daily_badge.date != today:
+        if unlocked_badges:
+            badge_id = random.choice(unlocked_badges)
+            daily_badge.badge = Badge.objects.get(id=badge_id)
+        else:
+            daily_badge.badge = None
+        daily_badge.date = today
+        daily_badge.save()
 
+    return daily_badge
 
 #알람
 "notifi/"
 class AlarmView(generics.ListAPIView):
+    permission_classes=[IsAuthenticated]
+
     serializer_class=NotificationSerializer
     def get_queryset(self):
-        return Notification.objects.filter(user=User.objects.get(id=1), is_read=False) ##request.user로 변경 필요
-
-
-#소감 작성
-"goalAchieve/<int:goal_id>/"
-class GoalAchieveView(generics.ListCreateAPIView):
-    serializer_class=GoalAchievementSerializer
-
-    def perform_create(self, serializer):
-        # user = self.request.user
-        user = User.objects.get(id=1)
-        goal_id = self.kwargs.get('goal_id') 
-        badge_id = self.request.data.get('badge_id') 
-        feedback = self.request.data.get('feedback', '')  
-
-        try:
-            goal = Goal.objects.get(id=goal_id)
-        except Goal.DoesNotExist:
-            raise serializers.ValidationError("목표가 존재하지 않습니다.")
-
-        if badge_id:
-            try:
-                badge = Badge.objects.get(id=badge_id)
-            except Badge.DoesNotExist:
-                raise serializers.ValidationError("뱃지가 존재하지 않습니다.")
-        else:
-            badge = None
-
-        # 기존 목표 달성 기록이 있는지 확인하고, 있으면 업데이트
-        user_goal_achievement, created = GoalAchievement.objects.update_or_create(
-            user=user,
-            achieved_goal=goal,
-            defaults={'badge': badge, 'feedback': feedback}
-        )
-        
-        return user_goal_achievement
+        return Notification.objects.filter(user=self.request.user, is_read=False)
