25253afff2d1e6374b326fc59de42aac7835e1e3
seo hyun seok <tjgustjr33@naver.com>
2024-08-05 16:47:40 +0900

    refactor : todolists models
    투두리스트 생성시 user_id 가 고정적으로 1인 문제 수정

--- todolists/models.py
+++ todolists/models.py
@@ -7,4 +7,4 @@ class Todo(models.Model):
     date = models.DateField(default=date.today)
     time = models.TimeField()
     content = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todolists', default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todolists')
